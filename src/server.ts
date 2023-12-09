809e5ee3a43f251a386ffe993f98c5f673a71d7a
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-12-10 02:42:40 +0900

    Feat: Migrate to typescript

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 import path from "path";
 
-const app = express();
+const app: Express = express();
 const PORT = 3000;
 const __dirname = path.resolve();
 
 app.use("/src", express.static(path.resolve(__dirname, "src")));
 
-app.get("/*", (req, res) => {
+app.get("/*", (req: Request, res: Response) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
